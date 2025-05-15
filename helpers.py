74fbf8178894ac79b6862fb68f98868b01d490ec
fh <128391719+filsan-1@users.noreply.github.com>
2025-05-15 15:04:32 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,2 +1,7 @@
-relative = lambda landmark, shape: (int(landmark.x * shape[1]), int(landmark.y * shape[0]))
-relativeT = lambda landmark, shape: (int(landmark.x * shape[1]), int(landmark.y * shape[0]), 0)
+def relative(point, shape):
+    h, w = shape[:2]
+    return (int(point.x * w), int(point.y * h))
+
+def relativeT(point, shape):
+    h, w = shape[:2]
+    return (point.x * w, point.y * h, point.z * w)
